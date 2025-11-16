71072d3a116f10a725ee82137d29fb50396c86a4
polyviosmusic@gmail.com <polyviosmusic@gmail.com>
2025-11-16 13:40:05 +0100

    supplementary exercise

---
+++ lesson1/Supplementary_exercise.py
@@ -0,0 +1,7 @@
+name = "John"
+age = 32
+favorite_instrument = "theorbo"
+favorite_composer = "Kapsberger"
+country = "Austria"
+
+print(f"My name is {name}, I am {age}, I live in {country}, My favorite instrument is {favorite_instrument}, My favorite composer is {favorite_composer}.")
