dea591d403be6a141e7da8545282f2b931aca3a2
polyviosmusic@gmail.com <polyviosmusic@gmail.com>
2025-11-16 14:11:55 +0100

    2supplement

---
+++ lesson2/2supplementary_exercise.py
@@ -0,0 +1,16 @@
+style = input("What is your favorite musical period? ").lower()
+
+if style == "baroque":
+    print("You enjoy the elgance and beauty of the baroque era.")
+elif style == "classical":
+    print("You appreciate clarity, balance, and refined expression.")
+elif style == "romantic":
+    print("You feel deeply and enjoy expressive, emotionally driven music.")
+elif style == "jazz": 
+    print("I will forgive you. I suppose you appreciate improvisation and rhythmic freedom")
+elif style == "renaissance":
+    print("You and enjoy and are delighted by riveting polyphony.")
+elif style == "rock":
+    print("You are probably possessed by demons!.")
+else:
+    print("Good for you! Every musical style has its unique charm")
