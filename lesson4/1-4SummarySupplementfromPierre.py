8547b4b774726be0683747f9e18763082a134f0e
polyviosmusic@gmail.com <polyviosmusic@gmail.com>
2025-11-17 13:18:10 +0100

    Pierre supplement

---
+++ lesson4/1-4SummarySupplementfromPierre.py
@@ -0,0 +1,17 @@
+note = input ("Enter note:").lower()
+if note == "c":
+    print ("doh")
+if note == "d":
+    print ("re")
+if note == "ce":
+    print ("mi")
+if note == "f":
+    print ("mi")
+if note == "g":
+    print ("sol")
+if note == "a":
+    print ("la")
+if note == "b":
+    print ("si")
+
+print ("There you go Pierre!")
